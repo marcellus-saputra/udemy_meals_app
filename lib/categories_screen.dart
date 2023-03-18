0aff973bf957cc1d9094219e9b5057f30e992770
Marcellus Saputra <marcellussaputra@gmail.com>
2023-03-18 16:58:00 +0100

    Category screen theming/styling

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -2,18 +2,32 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/category_card.dart';
+
+import 'dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MEALs'),
       ),
-      children: [],
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((data) => CategoryCard(
+                  data.title,
+                  data.color,
+                ))
+            .toList(),
+      ),
     );
   }
 }
